3fff03dd4a2b5af5ce4330a7dcebebc56c603272
Jedonge <jedonge1995@gmail.com>
2025-11-24 00:53:38 +0900

    WIP: 임시 작업 저장

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       <Link href={"/sign-up"}>회원가입</Link>
+      <Link href={"/notification"}>알람</Link>
     </div>
   );
 }
